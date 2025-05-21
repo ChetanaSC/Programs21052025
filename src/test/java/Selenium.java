4569a322bcfba3757d067d4c5e9301564e2a66a2
chetana <scchetana@gmail.com>
2025-05-21 20:21:28 +0530

    Selenium Commit

---
+++ src/test/java/Selenium.java
@@ -0,0 +1,20 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Selenium {
+	@Test
+	public void test1()
+	{
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.xpath("S"));
+		driver.close();
+	}
+
+}
